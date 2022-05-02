6f8a9c84e6cbc2510abb26b66d83e7b6a8442146
Jay Schwane <jrschwane@uwalumni.com>
2022-05-01 21:06:56 -0500

    Simplify unit tests

--- src/234_palindrome-linked-list/index.spec.js
+++ src/234_palindrome-linked-list/index.spec.js
@@ -5,15 +5,13 @@ const { List } = require('../modules/singly-linked-list')
 describe('isPalindrome', () => {
   it('should return true for [1, 2, 2, 1]', () => {
     const list = List.makeListFromArray([1, 2, 2, 1])
-    const expected = true
     const actual = isPalindrome(list.head)
-    expect(actual).toEqual(expected)
+    expect(actual).toBe(true)
   })
 
   it('should return false for [1, 2]', () => {
     const list = List.makeListFromArray([1, 2])
-    const expected = false
     const actual = isPalindrome(list.head)
-    expect(actual).toEqual(expected)
+    expect(actual).toBe(false)
   })
 })
