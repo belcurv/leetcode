1f5c201ae5561937323890857235ac9068496874
Jay Schwane <jrschwane@uwalumni.com>
2022-07-16 10:56:25 -0500

    Complete Problem 4

--- src/0004_median-two-sorted-arrays/index.js
+++ src/0004_median-two-sorted-arrays/index.js
@@ -34,15 +34,110 @@ exports.naiveFindMedianSortedArrays = function naiveFindMedianSortedArrays (nums
 }
 
 /**
- * Binary search O(log (n + m))
+ * Binary search solution
+ * Time complexity: O( log(min(x,y) )
+ *
+ * Huge help: https://www.youtube.com/watch?v=LPFhl65R7ww
  *
  * @param  {number[]} nums1
  * @param  {number[]} nums2
  * @return {number}
  */
 exports.findMedianSortedArrays = function findMedianSortedArrays (nums1, nums2) {
-  // need to ensure nums1 is the shorter array. If it's longer, re-call with argument order swapped
+  /**
+   * Need to ensure nums1 is the shorter array. If nums1 is LONGER, re-call with argument order swapped.
+   * WHY?: We need to partition each array, and all positions are possible 'cut' locations in the shorter array.
+   * Whereas, if we started with the longer array, we could possiblly cut @ a position not exists in shorter array.
+  */
   if (nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1)
 
-  // todo
+  /**
+   * == Partitioning Explained ==
+   *
+   * Start by partitioning in the middle of 1st array (2nd array cut position is derived from 1st)
+   *                 v
+   *   nums1    1  4 | 6  10
+   *   nums2    3  8 | 9  11
+   *
+   * Move partitions in opposite directions until all LEFT values are less than all RIGHT values
+   *                     v
+   *   nums1    1   4  6 | 10
+   *   nums2    3 | 8  9   11
+   *              ^
+   *
+   * Check: are all LEFT vals < all RIGHT vals? YES: we've found the correct cut positions
+   *   nums1    1  4  6 | 10
+   *   nums2          3 | 8  9  11
+   */
+
+  const nums1Length = nums1.length
+  const nums2Length = nums2.length
+
+  /**
+   * Cut position limits = the extremes available to nums1 array.
+   * If we ever reach these limits, it means that ALL nums in one array are more/less than the other array.
+   */
+  let lowLimit = 0
+  let highLimit = nums1Length
+
+  while (lowLimit <= highLimit) {
+    /**
+     * Halve search space in each iteration.
+     * We cut nums1 in half, within search limits. nums2 position is based on nums1 cut position:
+     *   Given nums1.length: m
+     *   Given nums2.length: n
+     *   nums1 cut position: i,
+     *   nums2 cut position: (m + n + 1)/2 - i
+     */
+    const nums1CutPosition = Math.floor((lowLimit + highLimit) / 2)
+    const nums2CutPosition = Math.floor((nums1Length + nums2Length + 1) / 2 - nums1CutPosition)
+
+    /**
+     * Find nums1 left max & right min values for later comparison checks.
+     * NOTE: we will be comparing "diagonally":
+     *    nums1MaxLeftValue <> nums2MinRightValue
+     *    nums2MaxLeftValue <> nums1MinRightValue
+     */
+    const nums1MaxLeftValue = nums1CutPosition === 0 // if we reach lower bound ...
+      ? Number.NEGATIVE_INFINITY // no available values to left, set to -infinity
+      : nums1[nums1CutPosition - 1]
+    const nums1MinRightValue = nums1CutPosition === nums1Length // if we reach upper bound ...
+      ? Number.POSITIVE_INFINITY // no available values to right, set to +infinity
+      : nums1[nums1CutPosition]
+
+    // find nums2 left max & right min values
+    const nums2MaxLeftValue = nums2CutPosition === 0 // if we reach lower bound ...
+      ? Number.NEGATIVE_INFINITY // no available values to left, set to -infinity
+      : nums2[nums2CutPosition - 1]
+    const nums2MinRightValue = nums2CutPosition === nums2Length // if we reach upper bound ...
+      ? Number.POSITIVE_INFINITY // no available values to right, set to +infinity
+      : nums2[nums2CutPosition]
+
+    // diagnostics
+    // console.log(
+    //   { lowLimit, highLimit }, '\n',
+    //   'nums1 sets', nums1.slice(0, nums1CutPosition), nums1.slice(nums1CutPosition), '\n',
+    //   'nums2 sets', nums2.slice(0, nums2CutPosition), nums2.slice(nums2CutPosition)
+    // )
+
+    /**
+     * Comparisons
+     * When solution is NOT found, we +/- the high and low limits.
+     * Since the current cut positions are incorrect, we know we can move the limits to +/- 1 beyond
+     * the current cut positions.
+     */
+    if (nums1MaxLeftValue <= nums2MinRightValue && nums2MaxLeftValue <= nums1MinRightValue) { // arrays are partitioned correctly
+      if ((nums1Length + nums2Length) % 2 === 0) {
+        // when total # of elements is even, sum max left + min right and divide result by two
+        return (Math.max(nums1MaxLeftValue, nums2MaxLeftValue) + Math.min(nums1MinRightValue, nums2MinRightValue)) / 2
+      } else {
+        // when total # of elements is odd, median is just the max left value
+        return Math.max(nums1MaxLeftValue, nums2MaxLeftValue)
+      }
+    } else if (nums1MaxLeftValue > nums2MinRightValue) { // decrement high limit
+      highLimit = nums1CutPosition - 1
+    } else { // increment low limit
+      lowLimit = nums1CutPosition + 1
+    }
+  }
 }
--- src/0004_median-two-sorted-arrays/index.spec.js
+++ src/0004_median-two-sorted-arrays/index.spec.js
@@ -1,7 +1,7 @@
 
-const { naiveFindMedianSortedArrays } = require('./index')
+const { naiveFindMedianSortedArrays, findMedianSortedArrays } = require('./index')
 
-describe('naiveFindMedianSortedArrays', () => {
+describe('naiveFindMedianSortedArrays (brute force)', () => {
   // Explanation: merged array = [1,2,3] and median is 2.
   it('should return 2', () => {
     const nums1 = [1, 3]
@@ -17,6 +17,100 @@ describe('naiveFindMedianSortedArrays', () => {
     const nums2 = [3, 4]
     const expected = 2.5
     const actual = naiveFindMedianSortedArrays(nums1, nums2)
+    expect(actual).toBe(expected)
+  })
+
+  // Explanation: merged array = [1, 3, 4, 6, 8, 9, 10, 11] and median is (6 + 8) / 2 = 7
+  it('should return 7', () => {
+    const nums1 = [1, 4, 6, 10]
+    const nums2 = [3, 8, 9, 11]
+    const expected = 7
+    const actual = naiveFindMedianSortedArrays(nums1, nums2)
+    expect(actual).toBe(expected)
+  })
+})
+
+describe('findMedianSortedArrays (binary search)', () => {
+  // Explanation: merged array = [1,2,3] and median is 2.
+  it('should return 2', () => {
+    const nums1 = [1, 3]
+    const nums2 = [2]
+    const expected = 2
+    const actual = findMedianSortedArrays(nums1, nums2)
+    expect(actual).toBe(expected)
+  })
+
+  // Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+  it('should return 2.5', () => {
+    const nums1 = [1, 2]
+    const nums2 = [3, 4]
+    const expected = 2.5
+    const actual = findMedianSortedArrays(nums1, nums2)
+    expect(actual).toBe(expected)
+  })
+
+  it('should return 7', () => {
+    const nums1 = [1, 4, 6, 10]
+    const nums2 = [3, 8, 9, 11]
+    const expected = 7
+    const actual = findMedianSortedArrays(nums1, nums2)
+    expect(actual).toBe(expected)
+  })
+
+  /**
+   * Partitioning steps:
+   *
+   * nums1 sets [ 1, 3 ] [ 8, 9, 15 ]
+   * nums2 sets [ 7, 11, 18, 19 ] [ 21, 25 ]
+   *
+   * nums1 sets [ 1, 3, 8, 9 ] [ 15 ]
+   * nums2 sets [ 7, 11 ] [ 18, 19, 21, 25 ]
+   */
+  it('should return 11', () => {
+    const nums1 = [1, 3, 8, 9, 15]
+    const nums2 = [7, 11, 18, 19, 21, 25]
+    const expected = 11 // [1, 3, 7, 8, 9, 11, 15, 18, 19, 21, 25]
+    const actual = findMedianSortedArrays(nums1, nums2)
+    expect(actual).toBe(expected)
+  })
+
+  /**
+   * Partitioning scenario: all nums1 > all nums2
+   *
+   * nums1 sets [ 26 ] [ 27, 28 ]
+   * nums2 sets [ 7, 11, 18 ] [ 19 ]
+   *
+   * nums1 sets [] [ 26, 27, 28 ]
+   * nums2 sets [ 7, 11, 18, 19 ] []
+   */
+  it('should return 19', () => {
+    const nums1 = [26, 27, 28]
+    const nums2 = [7, 11, 18, 19]
+    const expected = 19 // [7, 11, 18, 19, 26, 27, 28]
+    const actual = findMedianSortedArrays(nums1, nums2)
+    expect(actual).toBe(expected)
+  })
+
+  /**
+   * Partitioning scenario: all nums1 < all nums2
+   *
+   * { lowLimit: 0, highLimit: 3 }
+   * nums1 sets [ 1 ] [ 3, 5 ]
+   * nums2 sets [ 7, 11, 18 ] [ 19 ]
+   *
+   * { lowLimit: 2, highLimit: 3 }
+   * nums1 sets [ 1, 3 ] [ 5 ]
+   * nums2 sets [ 7, 11 ] [ 18, 19 ]
+   *
+   * { lowLimit: 3, highLimit: 3 }
+   * nums1 sets [ 1, 3, 5 ] []
+   * nums2 sets [ 7 ] [ 11, 18, 19 ]
+   */
+  it('should return 19', () => {
+    const nums1 = [1, 3, 5]
+    const nums2 = [7, 11, 18, 19]
+    const expected = 7 // [1, 3, 5, 7, 11, 18, 19]
+    const actual = findMedianSortedArrays(nums1, nums2)
     expect(actual).toBe(expected)
   })
 })
